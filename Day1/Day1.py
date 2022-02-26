bfb9c4ebcae8e65753ed7c0c5e60d6711637e3e1
Godson Oladipupo <godsonoladipupo6@gmail.com>
2022-02-26 23:09:10 +0100

    Finished building Band name Generator

---
+++ Day1/Day1.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name Generator.")
+city_name = input("What is the name of the city you grew up in?\n")
+pet_name = input("What is the name of your pet?\n")
+print("The band name could be " + city_name + " " + pet_name + ".")
