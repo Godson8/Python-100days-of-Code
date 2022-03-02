26102ca1cdd63a9861895dd2f43a33d354560db9
Godson Oladipupo <godsonoladipupo6@gmail.com>
2022-03-03 00:28:54 +0100

    Finished Day4 Rock, Paper, Scissors

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -23,24 +23,36 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-computer_choice = random.randint(0, 2)
+computer_choice = (random.randint(0, 2))
 
-player_choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
+player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
 
 
-if player_choice == "0":
-    print(rock)
-elif player_choice == "1":
-    print(paper)
-elif player_choice == "2":
-    print(scissors)
+if player_choice == 0:
+    print(f"You chose Rock {rock}")
+elif player_choice == 1:
+    print(f"You chose Paper {paper}")
+elif player_choice == 2:
+    print(f"You chose Scissors {scissors}")
 else:
     print("Please follow the rules of the game.")
+    quit()
 
 
-if computer_choice == "0":
-    print(paper)
-elif computer_choice == "1":
-    print(scissors)
-elif computer_choice == "2":
-    print(rock)
+if computer_choice == 0:
+    print(f"Computer chose rock {rock}")
+elif computer_choice == 1:
+    print(f"Computer chose paper {paper}")
+elif computer_choice == 2:
+    print(f"Computer chose Scissors {scissors}")
+
+if computer_choice == 0 and player_choice == 2:
+    print("You Lose")
+elif player_choice == 0 and computer_choice == 2:
+    print("You win")
+elif player_choice > computer_choice:
+    print("You win")
+elif player_choice == computer_choice:
+    print("Its a draw")
+else:
+    print("You lose")
